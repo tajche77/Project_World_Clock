674e556c1112033469c8d112719b62202a823538
tajche77 <bozic.tamara@gmail.com>
2024-05-10 14:09:58 +0200

    corrected the code in js

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@ function updateCity(event) {
    </div>
    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
     "A"
-  )}</small>
+  )}</small></div>
    </div>
    <a href="/">All cities</a>
    `;
